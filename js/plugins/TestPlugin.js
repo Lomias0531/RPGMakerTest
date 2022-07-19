e7d41ceda9e846eeeda70b048ba604a68f88eb9a
Lomias0531 <56434685+Lomias0531@users.noreply.github.com>
2022-07-19 23:42:45 +0800

    update

--- js/plugins/TestPlugin.js
+++ js/plugins/TestPlugin.js
@@ -17,3 +17,25 @@ var Add_Buff = function (buff, strength) {
 class ThisBuff {
     
 }
+
+Game_Action.prototype.makeDamageValue = function (target, critical) {
+    const item = this.item();
+    const baseValue = this.evalDamageFormula(target);
+    let value = baseValue * this.calcElementRate(target);
+    if (this.isPhysical()) {
+        value *= target.pdr;
+    }
+    if (this.isMagical()) {
+        value *= target.mdr;
+    }
+    if (baseValue < 0) {
+        value *= target.rec;
+    }
+    if (critical) {
+        value = this.applyCritical(value);
+    }
+    value = this.applyVariance(value, item.damage.variance);
+    value = this.applyGuard(value, target);
+    value = Math.round(value);
+    return value;
+};

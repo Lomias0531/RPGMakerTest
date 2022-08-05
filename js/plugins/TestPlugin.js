617b58f5f2e6d8a56588a6e5f8c80ebbe5e5c343
Lomias0531 <460492314@qq.com>
2022-08-05 17:09:09 +0800

    Update TestPlugin.js

--- js/plugins/TestPlugin.js
+++ js/plugins/TestPlugin.js
@@ -3,12 +3,12 @@
 //=============================================================================
 
 /*:
- * @plugindesc Test plugin
+ * @plugindesc This is a Tool plugin
  * @author Lomias
  *
  * @help
  *
- * HELP!!!!
+ * You know this is a toolkit
  * 
  * @param ItemID
  * @desc Item to check
@@ -19,6 +19,8 @@
  * @default 0
  *
  */
+
+//检测单个物品是否满足需求数量，id为物品id，num为需求数量
 var CheckSingleItemRequired = function (id, num) {
     if ($gameParty.numItems($dataItems[id]) >= num) {
         return true;
@@ -27,6 +29,7 @@ var CheckSingleItemRequired = function (id, num) {
     }
 }
 
+//检测一系列物品是否满足数量需求，以字符串形式传递参数，规则为"物品1ID,物品1数量|物品2ID，物品2数量"
 var CheckItemListRequired = function (items) {
     var arr = str.split('|');
     for (var i = 0; i < arr.length; i++)
@@ -39,6 +42,7 @@ var CheckItemListRequired = function (items) {
     return true;
 }
 
+//提交任务所需物品，获得奖励，规则同上
 var SubmitQuestRequestedItems = function (items, rewards) {
     var itemsArr = items.split('|');
     var requestFulfilled = true;

f0c608e624eea79f66c06f4e71e4e22555a59522
Lomias0531 <460492314@qq.com>
2022-08-05 10:00:20 +0800

    update plugin

--- js/plugins/TestPlugin.js
+++ js/plugins/TestPlugin.js
@@ -28,7 +28,6 @@ var CheckSingleItemRequired = function (id, num) {
 }
 
 var CheckItemListRequired = function (items) {
-    let str = items;
     var arr = str.split('|');
     for (var i = 0; i < arr.length; i++)
     {
@@ -36,6 +35,32 @@ var CheckItemListRequired = function (items) {
         if ($gameParty.numItems($dataItems[arr1[0]]) < arr1[1]) {
             return false;
         }
+    }
+    return true;
+}
+
+var SubmitQuestRequestedItems = function (items, rewards) {
+    var itemsArr = items.split('|');
+    var requestFulfilled = true;
+    for (var i = 0; i < itemsArr.length; i++) {
+        var singleItemRequired = itemsArr[i].split(',');
+        if ($gameParty.numItems($dataItems[singleItemRequired[0]]) < singleItemRequired[1]) {
+            requestFulfilled = false;
+            break;
+        }
+    }
+
+    if (!requestFulfilled) return false;
+
+    for (var i = 0; i < itemsArr.length; i++) {
+        var singleItemRequired = itemsArr[i].split(',');
+        $gameParty.loseItem($dataItems[singleItemRequired[0]], Number(singleItemRequired[1]), false);
+    }
+
+    var rewardsArr = rewards.split('|');
+    for (var i = 0; i < rewardsArr.length; i++) {
+        var singleReward = rewardsArr[i].split(',');
+        $gameParty.gainItem($dataItems[singleReward[0]], Number(singleReward[1]), false);
     }
     return true;
 }

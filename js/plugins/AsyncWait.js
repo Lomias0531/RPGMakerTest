8695475d79d388321bd01e3b310d8a4ae0deae07
wswn99 <973692410@qq.com>
2022-08-15 17:49:50 +0800

    plugin

--- js/plugins/AsyncWait.js
+++ js/plugins/AsyncWait.js
@@ -2,14 +2,15 @@
 // AsyncWait
 //=============================================================================
 /*:
- * @plugindesc This plugin offer a function to make an async waiting.
+ * @plugindesc This plugin offer functions to give an event gradient opactiy.
  * @author BreadJI
  *
  * @help
  *
  *
  * functions list:
- *      waitingFor(time);
+ *      opactiy_Gradient_speed(id,opac,speed)
+ *      opactiy_Gradient(id,opac)   default
  *
  * @param time
  * @type number
@@ -17,13 +18,35 @@
  * @default 0
  *
  */// JavaScript source code
-async function waitingFor(time) {
-    var x = await f1(time);
-}
-function f1(x) {
+function f1(id,opac) {
+    $gameMap.events()[id-1]._opacity = opac;
     return new Promise(resolve => {
         setTimeout(() => {
-            resolve(x);
-        }, x);
+            resolve();
+        }, 100);
     });
+}
+
+
+//异步改变不透明度,参数为事件id，目标透明度（0-255），渐变速度（毫秒）
+async function opactiy_Gradient_Speed(id,opac,speed) {
+    var opac_old = $gameMap.events()[id - 1]._opacity;
+    var opac_differ = (opac - opac_old) / (speed / 100);
+
+
+    for (var i = 0; i < parseInt(speed / 100); i++) {
+        await f1(id, opac_old + opac_differ);
+        opac_old = opac_old + opac_differ;
+    }
+}
+//异步改变不透明度，渐变速度缺省，固定为1000毫秒
+async function opactiy_Gradient(id,opac) {
+    var opac_old = $gameMap.events()[id - 1]._opacity;
+    var opac_differ = parseInt((opac - opac_old) / 10);
+    
+    for (var i = 0; i < 10; i++) {
+        await f1(id, opac_old + opac_differ);
+        
+        
+    }
 }
